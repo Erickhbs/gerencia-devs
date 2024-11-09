0230de1fbc92529ff1fa37d099318f7c64d25222
Erickhbs <ehbs.13579@gmail.com>
2024-11-09 01:26:33 -0300

    conexao com o banco mysql e inicio da implementacao de cadastro dos associados

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
+<?php 
+    /* conexao com o MySQL para usar como bd */
+    //data source name ou dsn
+    $dsn = "mysql:host=localhost;dbname=gerencia_devs";
+    //usarei nome do usuario e senha padroes que vem jinto ao software XAMPP
+    $dbusername = "root";
+    $dbpassword = "";
 
+    try {
+        $pdo = new PDO($dsn, $dbusername, $dbpassword);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo 'falha na conexao'. $e->getMessage();
+    }
+
+?>
+
+<?php
+    /* todas as funcoes da aplicacao */
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,9 +32,15 @@
         </style>
     </head>
     <body>
-        <?php
-            echo 'ola mundo';
-        ?>
+        <h3>Cadastrar Associado</h3>
+
+        <form action="" method="post">
+            <input type="text" name="nome" placeholder="nome e sobrenome"><br>
+            <input type="email" name="email" placeholder="email do associado"><br>
+            <input type="text" name="cpf" placeholder="cpf do associado"><br>
+            <button>Cadastrar</button>
+        </form>
+
     </body>
 </html>
 
